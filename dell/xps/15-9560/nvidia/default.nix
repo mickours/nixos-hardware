65d3b0df6ecc796d029ccf4e0980463e337706a8
Michael Mercier <michael.mercier@ryax.tech>
2021-10-30 09:54:32 +0200

    Fix nvidia prime options for dell xps 15-9560

--- dell/xps/15-9560/nvidia/default.nix
+++ dell/xps/15-9560/nvidia/default.nix
@@ -13,8 +13,8 @@
   ##### disable intel, run nvidia only and as default
   services.xserver.videoDrivers = lib.mkDefault ["nvidia"];
   hardware.nvidia.modesetting.enable = lib.mkDefault true;
-  hardware.nvidia.optimus_prime.enable = lib.mkDefault true;
-  hardware.nvidia.optimus_prime.nvidiaBusId = lib.mkDefault "PCI:1:0:0";
-  hardware.nvidia.optimus_prime.intelBusId = lib.mkDefault "PCI:0:2:0";
+  hardware.nvidia.prime.sync.enable = lib.mkDefault true;
+  hardware.nvidia.prime.nvidiaBusId = lib.mkDefault "PCI:1:0:0";
+  hardware.nvidia.prime.intelBusId = lib.mkDefault "PCI:0:2:0";
 
 }
